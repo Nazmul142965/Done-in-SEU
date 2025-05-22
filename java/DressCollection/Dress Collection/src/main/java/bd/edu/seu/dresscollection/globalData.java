ffea9f598915747e03dd4219ef873108d3a8b344
Nazmul <afnannazmul@gmail.com>
2025-05-22 10:53:19 +0600

    Delete java/DressCollection directory

--- java/DressCollection/Dress Collection/src/main/java/bd/edu/seu/dresscollection/globalData.java
+++
@@ -1,8 +0,0 @@
-package bd.edu.seu.dresscollection;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-public class globalData {
-    public static ObservableList<tableDetails> tableDetailsObservableList = FXCollections.observableArrayList();
-}
--- java/DressCollection/Dress Collection/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module bd.edu.seu.dresscollection {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens bd.edu.seu.dresscollection to javafx.fxml;
-    exports bd.edu.seu.dresscollection;
-}
